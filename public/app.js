1bd6f643d369d3c6b66574ea6086bd5f08b66f78
Al-bat-ros <al-bat-ros@mail.ru>
2018-04-04 10:56:40 +0500

    Play with localStorage variables

--- public/app.js
+++ public/app.js
@@ -1,4 +1,10 @@
 function something()
 	{
-		alert('Hello everyone!');
+        
+        var x = localStorage.getItem('aaa');
+
+        window.localStorage.setItem('aaa',111);
+
+		alert(x);
+
 	}
